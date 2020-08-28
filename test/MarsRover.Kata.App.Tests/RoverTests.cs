992c4fb7b5a7b7b882d406a27b0bbe15fd9b56d8
Liam Beeton <liam.beeton@gmail.com>
2020-08-28 13:59:43 +0200

    feat(coordinate): rover move forward

--- test/MarsRover.Kata.App.Tests/RoverTests.cs
+++ test/MarsRover.Kata.App.Tests/RoverTests.cs
@@ -4,11 +4,13 @@ namespace MarsRover.Kata.App.Tests
 {
     public class RoverTests
     {
-        private readonly Rover _rover;
+        private readonly Grid grid;
+        private readonly Rover rover;
 
         public RoverTests()
         {
-            _rover = new Rover();
+            grid = new Grid(10, 10);
+            rover = new Rover(grid);
         }
 
         [Theory]
@@ -18,7 +20,7 @@ namespace MarsRover.Kata.App.Tests
         [InlineData("RRRR", "0 0 N")]
         public void RotateRight_Orientation_Calculated(string commands, string position)
         {
-            Assert.Equal(position, _rover.Execute(commands));
+            Assert.Equal(position, rover.Execute(commands));
         }
 
         [Theory]
@@ -28,7 +30,55 @@ namespace MarsRover.Kata.App.Tests
         [InlineData("LLLL", "0 0 N")]
         public void RotateLeft_Orientation_Calculated(string commands, string position)
         {
-            Assert.Equal(position, _rover.Execute(commands));
+            Assert.Equal(position, rover.Execute(commands));
+        }
+
+        [Theory]
+        [InlineData("M", "0 1 N")]
+        [InlineData("MMM", "0 3 N")]
+        public void MoveUp_Position_Calculated(string commands, string position)
+        {
+            Assert.Equal(position, rover.Execute(commands));
+        }
+
+        [Theory]
+        [InlineData("MMMMMMMMMM", "0 0 N")]
+        [InlineData("MMMMMMMMMMMMMMM", "0 5 N")]
+        public void MoveUp_WrapTopToBottom_Calculated(string commands, string position)
+        {
+            Assert.Equal(position, rover.Execute(commands));
+        }
+
+        [Theory]
+        [InlineData("RM", "1 0 E")]
+        [InlineData("RMMMMM", "5 0 E")]
+        public void MoveRight_Position_Calculated(string commands, string position)
+        {
+            Assert.Equal(position, rover.Execute(commands));
+        }
+
+        [Theory]
+        [InlineData("RMMMMMMMMMM", "0 0 E")]
+        [InlineData("RMMMMMMMMMMMMMMM", "5 0 E")]
+        public void MoveRight_WrapLeftToRight_Calculated(string commands, string position)
+        {
+            Assert.Equal(position, rover.Execute(commands));
+        }
+
+        [Theory]
+        [InlineData("LM", "9 0 W")]
+        [InlineData("LMMMMM", "5 0 W")]
+        public void MoveLeft_Position_Calculated(string commands, string position)
+        {
+            Assert.Equal(position, rover.Execute(commands));
+        }
+
+        [Theory]
+        [InlineData("LLM", "0 9 S")]
+        [InlineData("LLMMMMM", "0 5 S")]
+        public void MoveDown_Position_Calculated(string commands, string position)
+        {
+            Assert.Equal(position, rover.Execute(commands));
         }
     }
 }
